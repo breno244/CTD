77b65f67bbd9c05ce3ea0bcae9c0a560ce0552ca
Breno h sabino <53581540+breno244@users.noreply.github.com>
2021-10-27 20:54:53 -0300

    Add files via upload

---
+++ CTD/Programacao_imp/aula04/aula4.js
@@ -0,0 +1,23 @@
+console.log("Olá mundo")
+
+var x = 8; // isso é uma criação de variável
+var y = 5;
+const z = 25;// tipo constante
+x = 5; // atribui outo valor a variavel
+let h = 96;
+h = 78;
+
+ let nome = "Breno"; // tipo de string
+let plano = true; //booleano
+
+
+
+let soma = h + x;
+
+let sub = h - x;
+
+let mult = h * x;
+
+let div = h / x;
+
+console.log( x, y, z, h, nome, plano, soma, sub, mult, div );
---
+++ CTD/Programacao_imp/aula04/mesa4.js
@@ -0,0 +1,9 @@
+const nome = "Jose da Silva"
+var idade = 27
+var peso = 83.5
+let autura = 1.70
+let plano = true
+let imc = peso / (autura * autura)
+
+console.log(nome,"tem", idade, " anos e seu índice de massa corporal é de", imc)
+
